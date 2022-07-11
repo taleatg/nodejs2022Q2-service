f33391019c923b7ff643aae6e41af30ecab37e65
taleatg <t_korbyt@mail.ru>
2022-07-12 01:34:07 +0300

    init: start task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
